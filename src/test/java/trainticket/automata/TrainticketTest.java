90c63a70442793014dbc1ca82e684f52a7d2672c
Verbőczy Kristóf <verbocika@gmail.com>
2017-10-02 22:04:31 +0200

    Generated interface and its implementation

---
+++ src/test/java/trainticket/automata/TrainticketTest.java
@@ -0,0 +1,165 @@
+package trainticket.automata;
+
+import org.graphwalker.core.machine.ExecutionContext;
+import org.graphwalker.java.annotation.GraphWalker;
+
+@GraphWalker(start = "Start")
+public class TrainticketTest extends ExecutionContext implements TrainticketTester {
+
+	@Override
+	public void e_returnToWaiting() {
+		// TODO Auto-generated method stub
+		System.out.println("asd");
+	}
+
+	@Override
+	public void v_chooseFunction() {
+		// TODO Auto-generated method stub
+		System.out.println("asd");
+	}
+
+	@Override
+	public void e_toInvalid() {
+		// TODO Auto-generated method stub
+		System.out.println("asd");
+	}
+
+	@Override
+	public void e_creditcardSuccessful() {
+		// TODO Auto-generated method stub
+		System.out.println("asd");
+	}
+
+	@Override
+	public void e_whenInvalid() {
+		// TODO Auto-generated method stub
+		System.out.println("asd");
+	}
+
+	@Override
+	public void e_cash() {
+		// TODO Auto-generated method stub
+		System.out.println("asd");
+	}
+
+	@Override
+	public void e_cashError() {
+		// TODO Auto-generated method stub
+		System.out.println("asd");
+	}
+
+	@Override
+	public void e_toValid() {
+		// TODO Auto-generated method stub
+		System.out.println("asd");
+	}
+
+	@Override
+	public void v_to() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void e_creditCardError() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void e_chooseInternetTicket() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void v_from() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void e_fromInvalid() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void v_printTicket() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void v_creditcard() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void v_cash() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void e_fromValid() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void e_chooseTicketPurchase() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void e_validCode() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void e_invalidCode() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void v_when() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void v_internetTicket() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void e_whenValid() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void e_cashSuccessful() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void v_choosePaymentType() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void e_creditcard() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
