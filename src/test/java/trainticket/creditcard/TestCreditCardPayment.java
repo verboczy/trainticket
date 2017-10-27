b4b962a905e4655566d744895bc1ade3702cf9c7
Verbőczy Kristóf <verbocika@gmail.com>
2017-10-28 01:30:05 +0200

    Credit card payment unit test added

---
+++ src/test/java/trainticket/creditcard/TestCreditCardPayment.java
@@ -0,0 +1,51 @@
+package trainticket.creditcard;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class TestCreditCardPayment {
+	
+	private String name;
+	private String cardNumber;
+	private boolean expected;
+	private CreditCardPayment sut;
+	
+	public TestCreditCardPayment(String name, String cardNumber, boolean expected) {
+		
+		this.name = name;
+		this.cardNumber = cardNumber;
+		this.expected = expected;
+		
+		sut = new CreditCardPayment();
+	}
+	
+	@Parameters(name = "{0}")
+	public static List<Object[]> data() {
+		return Arrays.asList(new Object[][] {
+				 {"containsCharacter_False", "00000000-0000000a", false},
+				 {"tooShort_False", "0000000-0000000", false},
+				 {"tooFewBlocks_False", "00000000", false},
+				 {"tooLong_False", "000000000-000000000", false},
+				 {"tooManyBlocks_False", "00000000-00000000-00000000-00000000", false},
+				 {"twoBlocksValid_True", "00000000-00000000", true},
+				 {"threeBlocksValid_True", "00000000-00000000-00000000", true}
+		});
+	}
+	
+	@Test
+	public void testCreditCardNumberValidity() {
+		
+		boolean actual = sut.pay(cardNumber);
+		
+		assertEquals(expected, actual);
+	}
+	
+}
